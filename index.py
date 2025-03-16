6d2569af99202905dfa88a7eb72c89869e6d5de8
Nompumelelo <nompumelelopeninamthembu@gmail.com>
2025-03-16 14:02:07 +0200

    my first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+x = input("Enter x value:")
+y = input("Enter y value:")
+
+x = int(x)
+y = int(y)
+
+print(f"{x} + {y} = {x+y}")
+
+print(f"{x} - {y} = {x-y}")
+
+print(f"{x} * {y} = {x*y}")
+
+print(f"{x} / {y} = {x/y}")
+
+
